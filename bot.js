8ab3d7b44b70070c31a0271a3b30696d89a453b9
NeelFT <neelakshpeshin@gmail.com>
2026-07-10 20:56:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const bot = mineflayer.createBot({
   port: config.serverPort,
   username: config.botUsername,
   auth: 'offline',
-  version: "26.2",
+  version: "1.21.11",
   viewDistance: config.botChunk
 });
 
